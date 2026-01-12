8a88ba5e52a8600856f1e63a4185ec8da72a2c2b
Krushna Khairnar <krushnakhairnar536@gmail.com>
2026-01-12 22:14:06 +0530

    Setup DB connection and env variables

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { ENV } from "./config/env.js"; 
+import { connectDB } from "./config/db.js";
+
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
-const PORT = ENV.PORT || 5001;
-
 app.listen( ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`);
+    console.log(`Server is running on port ${ENV.PORT}`);
+    connectDB();
 });
