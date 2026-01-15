80f2822b1fb3af2e09bb150c69792c6e6cfc4b2d
Krushna Khairnar <krushnakhairnar536@gmail.com>
2026-01-15 13:42:57 +0530

    useStreamChat hook implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,40 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, SignInButton, useAuth, UserButton } from '@clerk/clerk-react';
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
 import * as Sentry from "@sentry/react";
+import CallPage from './pages/CallPage';
 
 const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
 const App = () => {
+  const { isSignedIn, isLoaded  } = useAuth();
+
+  if (!isLoaded) return null;
+
   return (
-    <>
+    <SentryRoutes>
+        <Route path="/" element={isSignedIn ? <HomePage /> : <Navigate to={"/auth"} replace />} />
+        <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />} />
+
+        <Route
+        path="/call/:id"
+        element={isSignedIn ? <CallPage /> : <Navigate to={"/auth"} replace />}/>
+
+        <Route
+        path="*"
+        element={isSignedIn ? <Navigate to={"/"} replace /> : <Navigate to={"/auth"} replace />}/>
+    </SentryRoutes>
+  );
+};
+
+export default App
+
+
+//First version of routing
+/*
+<>
       <SignedIn>
         <SentryRoutes>
             <Route path="/" element={<HomePage />}/>
@@ -24,7 +49,4 @@ const App = () => {
         </SentryRoutes>
       </SignedOut>
     </>
-  );
-};
-
-export default App
+*/
